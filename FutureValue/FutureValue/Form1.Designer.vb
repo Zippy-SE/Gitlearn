09d94f7b21a833e4d685d304825418a690bd3090
Zippy-SE <55605866+Zippy-SE@users.noreply.github.com>
2019-09-20 17:47:09 -0700

    Add files via upload

---
+++ FutureValue/FutureValue/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtMonthlyInvestment = New System.Windows.Forms.TextBox()
+        Me.txtInterestRate = New System.Windows.Forms.TextBox()
+        Me.txtYears = New System.Windows.Forms.TextBox()
+        Me.txtFutureValue = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Monthly Investment:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(103, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Yearly Interest Rate:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 89)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Number of Years:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(31, 119)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Future Value:"
+        '
+        'txtMonthlyInvestment
+        '
+        Me.txtMonthlyInvestment.Location = New System.Drawing.Point(157, 26)
+        Me.txtMonthlyInvestment.Name = "txtMonthlyInvestment"
+        Me.txtMonthlyInvestment.Size = New System.Drawing.Size(100, 20)
+        Me.txtMonthlyInvestment.TabIndex = 4
+        '
+        'txtInterestRate
+        '
+        Me.txtInterestRate.Location = New System.Drawing.Point(157, 55)
+        Me.txtInterestRate.Name = "txtInterestRate"
+        Me.txtInterestRate.Size = New System.Drawing.Size(100, 20)
+        Me.txtInterestRate.TabIndex = 5
+        '
+        'txtYears
+        '
+        Me.txtYears.Location = New System.Drawing.Point(157, 86)
+        Me.txtYears.Name = "txtYears"
+        Me.txtYears.Size = New System.Drawing.Size(100, 20)
+        Me.txtYears.TabIndex = 6
+        '
+        'txtFutureValue
+        '
+        Me.txtFutureValue.Location = New System.Drawing.Point(157, 116)
+        Me.txtFutureValue.Name = "txtFutureValue"
+        Me.txtFutureValue.ReadOnly = True
+        Me.txtFutureValue.Size = New System.Drawing.Size(100, 20)
+        Me.txtFutureValue.TabIndex = 7
+        Me.txtFutureValue.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(72, 172)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "&Calculate"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button2.Location = New System.Drawing.Point(182, 172)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "E&xit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.Button1
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Button2
+        Me.ClientSize = New System.Drawing.Size(288, 236)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtFutureValue)
+        Me.Controls.Add(Me.txtYears)
+        Me.Controls.Add(Me.txtInterestRate)
+        Me.Controls.Add(Me.txtMonthlyInvestment)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Future Value"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtMonthlyInvestment As TextBox
+    Friend WithEvents txtInterestRate As TextBox
+    Friend WithEvents txtYears As TextBox
+    Friend WithEvents txtFutureValue As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
